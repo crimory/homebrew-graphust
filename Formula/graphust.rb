f03fd5d9aa7cd415d720fd1bc8bb902205c77d44
Marcin Kern <silonpl@gmail.com>
2023-04-21 11:15:16 +0200

    Release 0.2.0

--- Formula/graphust.rb
+++ Formula/graphust.rb
@@ -2,8 +2,8 @@ class Graphust < Formula
     desc "Produce simple graphs from basic text input"
     homepage "https://github.com/crimory/graphust"
     url "https://github.com/crimory/graphust/releases/latest/download/graphust.tar.gz"
-    sha256 "57aa81ae7db8f9499c11b42c30e9b59442689028b9308e3447ef6cb61b2910af"
-    version "0.1.12"
+    sha256 "2c2634f759a00cb9d5d4d64098e4b7215cd3327c8aa2f97fd536ee88e7e1d9ee"
+    version "0.2.0"
 
     def install
         bin.install "graphust"
